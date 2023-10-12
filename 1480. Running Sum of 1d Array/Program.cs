f08715a5674a71ed632445856660ac488ef53db6
Pedro Soucheff <pedro.soucheff@live.com>
2023-10-12 09:51:19 -0300

    1480

---
+++ 1480. Running Sum of 1d Array/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Solution {
+       public int[] RunningSum(int[] nums) {
+            for(int i=1; i<nums.Length; i++){
+                nums[i]+=nums[i-1];
+            }
+            return nums;    
+        }
+    }
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] a = {1,2,3,4};
+            Solution s = new Solution();
+            Console.WriteLine("[{0}]",string.Join(",",s.RunningSum(a)));
+        }
+    }
+}
