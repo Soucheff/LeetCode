0f4153f53d73c5adeb58edcf9ecb8cfc7dd0a8ab
Pedro Soucheff <pedro.soucheff@live.com>
2023-10-12 10:17:38 -0300

    1672

---
+++ 1672. Richest Customer Wealth/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Solution
+    {
+        int max = 0;
+        public int MaximumWealth(int[][] accounts)
+        {
+            foreach(int[] account in accounts)
+            {
+                if(account.Sum() > max)
+                {
+                    max = account.Sum();
+                }
+            }
+            return max;
+        }
+    }
+    internal class Program
+    {
+        static void Main()
+        {
+            int[][] a = new int[][]{ 
+                                    new int[] {1, 2, 3}, 
+                                    new int[] {3, 2, 1} 
+                                };
+            Solution s = new Solution();
+            Console.WriteLine("{0}",s.MaximumWealth(a));
+        }
+    }
+}
