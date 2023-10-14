238babbe1c0114187352c0fd69d488bd8eca5ddb
Pedro Soucheff <pedro.soucheff@live.com>
2023-10-14 13:30:15 -0300

    383. Ransom Note

---
+++ 383. Ransom Note/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace App{
+
+    public class Solution {
+       public bool CanConstruct(string ransomNote, string magazine) {
+            if(ransomNote.Length > magazine.Length) return false;
+            int indexS = 0;
+            foreach(char l in ransomNote){
+                indexS = magazine.IndexOf(l);
+                if(indexS == -1){
+                    return false;
+                }else{
+                    magazine = magazine.Remove(indexS,1);
+                }
+            }
+            return true;
+        }
+    }
+    internal class Program{
+        static void Main(){
+            Solution s = new Solution();
+            Console.WriteLine(s.CanConstruct("aa","aab"));
+        }
+    }
+}
