0e1e39c4cac400070b90754139c8a3f5c1fef62a
Pedro Soucheff <pedro.soucheff@live.com>
2023-10-14 11:54:05 -0300

    1342

---
+++ 1342. Number of Steps to Reduce a Number to Zero/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace App{
+
+
+    public class Solution {
+        public int NumberOfSteps(int num) {
+            //Break point to avoid loop
+            int steps=0;
+            while(num>0){
+                steps++;
+                if(num % 2 == 0){
+                    Console.WriteLine("Step {0}) {1} is even; divide by 2 and obtain {2}.",steps,num,num/2);
+                    num = num/2;
+                    continue;
+                    
+                }else{
+                    Console.WriteLine("Step {0}) {1} is odd; subtract 1 and obtain {2}.",steps,num,num-1);
+                    num -= 1;
+                    continue;
+                }
+                
+            }
+            return steps;
+        }
+    }
+
+    internal class Program{
+
+        static void Main(){
+            Solution s = new Solution();
+            Console.WriteLine(s.NumberOfSteps(123));
+        }
+
+    }
+
+}
