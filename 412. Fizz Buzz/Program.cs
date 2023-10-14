7f1003b5bffc82fee5aeb34e3dd2379c38c55181
Pedro Soucheff <pedro.soucheff@live.com>
2023-10-14 11:21:49 -0300

    412. Fizz Buzz

---
+++ 412. Fizz Buzz/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace App {
+
+    public class Solution {
+
+        public IList<string> FizzBuzz(int n) {
+            List<string> l = new List<string>();
+
+            for(int i = 1 ; i <= n ; i++ ){
+                string result = "";
+            
+                if( i % 3 == 0){
+                    result = "Fizz";
+                }
+            
+                if( i % 5 == 0){
+                    result += "Buzz";
+                }
+            
+                l.Add( (result == "") ? i.ToString() : result );  
+            }   
+            return l;
+        }
+    }
+    internal class Program{
+        static void Main(){
+            Solution s = new Solution();
+            Console.WriteLine("[{0}]",string.Join(",",s.FizzBuzz(15)));
+
+        }
+    }
+}
+
+
